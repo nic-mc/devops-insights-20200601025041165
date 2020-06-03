6d65772afaf5562a9eea9886a3cb0ac15d563c0c
Nic Mcadam <nic.j.mcadam@hotmail.com>
2020-06-03 16:26:05 +1200

    COMPX341-20 A4 VERSION 1

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -7,9 +7,9 @@ function AppContainer(props) {
 
     const [responseData, setResponseData] = useState('');
 
-    const handleZipChange = async (zipValue) => {
+    const handleZipChange = async (city) => {
         //console.log(`--------- fetchData called zip:${zipValue}`)
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&q=${zipValue},nz`)
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&q=${city},nz`)
         const json = await res.json()
         //console.log(json);
         setResponseData(json);
